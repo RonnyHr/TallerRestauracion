3a3b60ae93d10983b9d2d245e47f958c9003959e
RonnyHr <ronny.dumancela@utc.edu.ec>
2025-05-21 21:35:40 -0500

    Listamos las entidades: Museo, Etnomusicologo e Intervencion

--- Aplicaciones/Gestion/views.py
+++ Aplicaciones/Gestion/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Museo, Etnomusicologo, Intervencion
 
 # Create your views here.
+
+#listar las entidades
+def listaMuseos(request):
+    museos = Museo.objects.all()
+    return render(request, "museo.html", {'museos': museos})
+
+def listaEtnomusicologos(request):
+     etnos = Etnomusicologo.objects.all()
+     return render(request, "etnomusicologo.html", {'etnomusicologos': etnos})
+
+def listaIntervenciones(request):
+    intervenciones = Intervencion.objects.all()
+    return render(request, "intervencion.html", {'intervenciones': intervenciones})
