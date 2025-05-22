b6dbd517c554c50305cf5481290de0da7ca88131
RonnyHr <ronny.dumancela@utc.edu.ec>
2025-05-21 21:44:05 -0500

    Guardar datos  para: Museo, Etnomusicologo, Intervencion

--- Aplicaciones/Gestion/views.py
+++ Aplicaciones/Gestion/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Museo, Etnomusicologo, Intervencion
+from django.contrib import messages
 
 # Create your views here.
 
@@ -27,3 +28,41 @@ def nuevaIntervencion(request):
     museos = Museo.objects.all()
     etnos = Etnomusicologo.objects.all()
     return render(request, "agregarIntervencion.html", {"museos": museos, "etnos": etnos})
+
+#Guardar los datos
+
+def guardarMuseo(request):
+    Museo.objects.create(
+        nombre=request.POST["nombre"],
+        pais=request.POST["pais"],
+        ciudad=request.POST["ciudad"],
+        direccion=request.POST["direccion"],
+        fundado_en=request.POST["fundado_en"]
+    )
+    messages.success(request, "MUSEO GUARDADO EXITOSAMENTE")
+    return redirect('/museos')
+
+def guardarEtnomusicologo(request):
+    Etnomusicologo.objects.create(
+        codigo_trabajador=request.POST["codigo_trabajador"],
+        nombre=request.POST["nombre"],
+        especialidad=request.POST["especialidad"],
+        email=request.POST["email"],
+        telefono=request.POST["telefono"]
+    )
+    messages.success(request, "ETNOMUSICOLOGO GUARDADO EXITOSAMENTE")
+    return redirect('/etnomusicologos')
+
+
+def guardarIntervencion(request):
+    etno = Etnomusicologo.objects.get(id=request.POST["etnomusicologo"])
+    museo = Museo.objects.get(id=request.POST["museo"])
+    Intervencion.objects.create(
+        etnomusicologo=etno,
+        museo=museo,
+        descripcion=request.POST["descripcion"],
+        fecha=request.POST["fecha"],
+        duracion_dias=request.POST["duracion_dias"]
+    )
+    messages.success(request, "INTERVENCIÓN GUARDADA EXITOSAMENTE")
+    return redirect('/intervenciones')
