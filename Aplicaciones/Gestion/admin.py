044fe6a4811cc8ea95b71bd568a90591a0c42ff4
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-05-22 00:03:49 -0500

    Creamos la estructura de base.html

--- Aplicaciones/Gestion/admin.py
+++ Aplicaciones/Gestion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Museo
+from .models import Etnomusicologo
+from .models import Intervencion
 
 # Register your models here.
+admin.site.register(Museo)
+admin.site.register(Etnomusicologo)
+admin.site.register(Intervencion)
